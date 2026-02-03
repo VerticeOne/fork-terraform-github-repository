719c146cc8fa2d882e39fbb1dae861a1bf43b8b9
Miroslav Zapletal <97033624+nuginy@users.noreply.github.com>
2026-02-03 10:11:14 +0100

    FEATURE ASH-1005: Created a new feature allowing deploying repository environments (#9)

--- variables.tf
+++ variables.tf
@@ -595,3 +595,33 @@ variable "github_actions_permissions" {
     enabled = false
   }
 }
+
+variable "environments" {
+  description = "Github Environments configuration for the repository. In deployment_branch_policy, both protected_branches and custom_branch_policies cannot be set to true at the same time."
+  type = map(object({
+    wait_timer          = optional(number, 0)
+    admin_bypass        = optional(bool, false)
+    prevent_self_review = optional(bool, false)
+    reviewers = optional(object({
+      users = optional(list(string), [])
+      teams = optional(list(string), [])
+    }), {})
+    deployment_branch_policy = optional(object({
+      protected_branches     = optional(bool, false)
+      custom_branch_policies = optional(bool, false)
+    }), {})
+    env_variables              = optional(map(string), {})
+    env_secrets                = optional(map(string), {})
+    branch_deployment_policies = optional(list(string), [])
+    tag_deployment_policies    = optional(list(string), [])
+    })
+  )
+  default = {}
+  validation {
+    condition = alltrue([
+      for env, cfg in var.environments :
+      !(cfg.deployment_branch_policy.protected_branches && cfg.deployment_branch_policy.custom_branch_policies)
+    ])
+    error_message = "Cannot set both protected_branches and custom_branch_policies to true in any environment."
+  }
+}
